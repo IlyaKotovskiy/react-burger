b160ca83574173fad70d50de78cb292fa10d76d7
IlyaKotovskiy <ilya.kot.web@gmail.com>
2025-07-12 19:26:13 +0300

    fix(basename and homapage): sdgdfg

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ export type AppDispatch = ThunkDispatch<
 
 root.render(
 	<StrictMode>
-		<Router basename='/'>
+		<Router basename='/react-burger'>
 			<Provider store={store}>
 				<App />
 			</Provider>
